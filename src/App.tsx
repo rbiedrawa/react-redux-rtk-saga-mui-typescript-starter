4b38a107be23a7ebe1a063708d970ec854654364
rbiedrawa <biedrawa.rafal@gmail.com>
2022-03-07 17:10:33 +0100

    feat(mui): Material Ui setup
    Added material ui:
    yarn add @mui/material @emotion/react @emotion/styled @mui/icons-material
    
    Added Album template to check rendering. https://mui.com/getting-started/templates/

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,63 @@
 import React from 'react';
 import './App.css';
+import {Button} from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from '@mui/material/AppBar';
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <AppBar position="relative">
+                <Toolbar>
+                    <LocalPostOfficeIcon sx={{ mr: 2 }} />
+                    <Typography variant="h6" color="inherit" noWrap>
+                        App
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <main>
+                <Box
+                    sx={{
+                        bgcolor: 'background.paper',
+                        pt: 2,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth="md">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            Posts
+                        </Typography>
+                        <Stack
+                            sx={{ pt: 4 }}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+                        >
+                            <Button variant="contained">Submit</Button>
+                            <Button variant="outlined">Back</Button>
+                        </Stack>
+                    </Container>
+                </Box>
+            </main>
+        </ThemeProvider>
+    );
 }
 
 export default App;
