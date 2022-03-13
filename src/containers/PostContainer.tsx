d6bcdb0e5b827248e4d8d56ea789630a999e4682
rbiedrawa <biedrawa.rafal@gmail.com>
2022-03-13 11:38:48 +0100

    feat(cleanup): Custom Hooks
    Added custom hook for decoupling saga from containers/components.

--- src/containers/PostContainer.tsx
+++ src/containers/PostContainer.tsx
@@ -1,35 +1,23 @@
 import React, {useEffect} from "react";
-import {useAppDispatch, useAppSelector} from "../store/hooks";
-import {postsActions, selectPosts} from "../store/features/posts/posts.slice";
 import PostList from "../components/Posts/PostList";
-import PostForm, {PostFormInput} from "../components/Posts/PostForm";
+import PostForm from "../components/Posts/PostForm";
 import Container from "@mui/material/Container";
-import {Post} from "../libs/models/post.model";
+import {usePostService} from "../hooks";
+
 
 const PostContainer = () => {
-    const dispatch = useAppDispatch();
-    const posts = useAppSelector(selectPosts);
+    const {posts, deletePost, updatePost, fetchAllPosts,createPost} = usePostService();
 
     useEffect(() => {
-        dispatch(postsActions.fetchAll());
+        fetchAllPosts();
     }, []);
-
-    const onAddPost = (data: PostFormInput) => dispatch(postsActions.create({title: data!.title, body: data!.body}));
-
-    const onDeletePost = (post: Post) => dispatch(postsActions.delete(post));
-
-    const onUpdatePost = (post: Post) => dispatch(postsActions.update({
-        ...post,
-        body: `Updated at ${new Date().toISOString()}`
-    }));
-
     return (
         <>
             <Container maxWidth="xs">
-                <PostForm onSubmitClick={onAddPost}/>
+                <PostForm onSubmitClick={createPost}/>
             </Container>
             <Container sx={{py: 4}} maxWidth="md">
-                <PostList posts={posts} onDeletePost={onDeletePost} onUpdatePost={onUpdatePost}/>
+                <PostList posts={posts} onDeletePost={deletePost} onUpdatePost={updatePost}/>
             </Container>
         </>
     );
